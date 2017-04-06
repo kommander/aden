da1d0ea185451d7363738d9ff678a0ad80399491
Sebastian Herrlinger <hasta84@gmail.com>
2017-04-06 04:53:59 +0200

    cleanup, chore, remove devapp

--- index.js
+++ index.js
@@ -11,9 +11,8 @@ program
   .option('-b, --build', 'Will only build out the app assets and exit (not start the server)')
   .option('-v, --verbose', 'Output a lot')
   .option('-s, --silent', 'Do not output anything on purpose')
-  // .option('-t, --test', 'Run tests')
-  // .option('--clean', 'Remove all loaded dependencies and tmp folders in the tree')
-  // .option('--focus', 'Choose one route to focus on. Mount only that.')
+  // TODO: .option('--clean', 'Remove all dist folders')
+  // TODO: .option('--focus', 'Choose one route to focus on. Mount only that.')
   .parse(process.argv);
 
 const logger = (new Logger({
@@ -38,10 +37,9 @@ app.program = program;
 
 // TODO: Hand over program options as config to bootstrap and then aden itself,
 //       >> Do not rely on app.program
-bootstrap(app, logger, {}).then((result) => {
-  const aden = result[0];
+bootstrap(app, logger, {}).then((aden) => {
   const port = parseInt(program.port, 10) || aden.rootPage.port || 3000;
   app.listen(port, () => logger.success(`Started server at port ${port}`));
 }).catch((err) => {
-  console.log('Fatal:', err);
+  logger.error('Fatal:', err);
 });
