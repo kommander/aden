3534b5c0825943f2eef27ea3b7b0bdadbcd6b123
Sebastian Herrlinger <hasta84@gmail.com>
2017-04-16 07:39:34 +0200

    update examples

--- examples/babel/index.jsx
+++
@@ -1,9 +0,0 @@
-const fn = (str) => {
-  let newStr = '';
-  for (let i = 0; i < 5; i++) {
-    newStr += str;
-  }
-  return newStr;
-};
-const ele = document.getElementById('test');
-ele.innerHTML = fn('yeah!');
--- examples/renderer/.get.js
+++ examples/renderer/.get.js
@@ -1,4 +1,3 @@
-// TODO: Ensure the .file includes are in page scope
-module.exports = (aden, page) => (req, res, page, data) => {
+module.exports = (/* aden, page */) => (req, res, page, data) => {
   res.json({ page, data });
 };
