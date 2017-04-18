732f4ddd8c92c83aad93921222f8751dcaaad739
Sebastian Herrlinger <hasta84@gmail.com>
2017-04-18 17:34:58 +0200

    add test for html in subfolders

--- test/integration/dev.js
+++ test/integration/dev.js
@@ -56,6 +56,44 @@ describe('dev', () => {
       });
   });
 
+  she('recognises new files in sub folders and sets up the page', (done) => {
+    aden().init(path.resolve(__dirname, '../tmpdata/dev'))
+      .then((an) => an.run('dev'))
+      .then((an) => {
+        request(an.app)
+          .get('/sub')
+          .end((err, res) => {
+            if (err) {
+              done(err);
+              return;
+            }
+            expect(res.status).toMatch(404);
+
+            fs.writeFileSync(
+              path.resolve(__dirname, '../tmpdata/dev/sub', 'index.html'),
+              '<tag>sub content</tag>'
+            );
+
+            // Mhhh... need to know when a build has finished
+            an.on('dev:reload:done', () => {
+              request(an.app)
+                .get('/sub')
+                // fckn hell. todo: use promisified supertest
+                .end((err2, res2) => {
+                  if (err2) {
+                    done(err2);
+                    return;
+                  }
+
+                  expect(res2.text).toMatch(/<tag>sub content<\/tag>/ig);
+
+                  an.shutdown(done);
+                });
+            });
+          });
+      });
+  });
+
   she('recognises changed watch keys', (done) => {
     aden().init(path.resolve(__dirname, '../tmpdata/dev2'))
       .then((an) => an.run('dev'))
