6016d5fe812aa219b46d9b8669ac658d0111c87f
Sebastian Herrlinger <hasta84@gmail.com>
2017-04-18 09:02:24 +0200

    add inject test

---
+++ test/integration/basics.js
@@ -0,0 +1,20 @@
+const aden = require('../../lib/aden');
+const path = require('path');
+const request = require('supertest');
+const expect = require('expect');
+
+describe('Basics', () => {
+  she('injects common.js into existing html', (done) => {
+    aden().init(path.resolve(__dirname, '../data/html'))
+      .then((an) => an.run('dev'))
+      .then((an) => {
+        request(an.app)
+          .get('/')
+          .end((err, res) => {
+            if (err) done(err);
+            expect(res.text).toMatch(/<script type="text\/javascript" src="\/commons\.js">/ig);
+            an.shutdown(done);
+          });
+      });
+  });
+});
