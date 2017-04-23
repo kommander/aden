c0130bfc1cee55a3562e8aa802dab47ae7211510
Sebastian Herrlinger <hasta84@gmail.com>
2017-04-23 20:28:38 +0200

    adjust tests to new behaviour

--- test/integration/css.js
+++ test/integration/css.js
@@ -4,30 +4,15 @@ const request = require('supertest');
 const expect = require('expect');
 
 describe('CSS Extension', () => {
-  she('does not inject global.js without a base.css', (done) => {
-    aden().init(path.resolve(__dirname, '../tmpdata/html'))
-      .then((an) => an.run('dev'))
-      .then((an) => {
-        request(an.app)
-          .get('/')
-          .end((err, res) => {
-            if (err) done(err);
-            expect(res.text).toNotMatch(/<script type="text\/javascript" src="\/global\.js">/ig);
-            an.shutdown(done);
-          });
-      })
-      .catch(done);
-  });
-
-  she('injects global.js with at least something like a base.css', (done) => {
+  she('puts base.css into commons', (done) => {
     aden().init(path.resolve(__dirname, '../tmpdata/cssbase'))
       .then((an) => an.run('dev'))
       .then((an) => {
         request(an.app)
-          .get('/')
+          .get('/commons.css')
           .end((err, res) => {
             if (err) done(err);
-            expect(res.text).toMatch(/<script type="text\/javascript" src="\/global\.js">/ig);
+            expect(res.text).toMatch(/\.aTestClass/ig);
             an.shutdown(done);
           });
       })
