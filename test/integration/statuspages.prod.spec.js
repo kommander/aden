53a7fa3819a75e8ba2bc4db3660a2b125d5ab9d9
Sebastian Herrlinger <hasta84@gmail.com>
2017-05-04 22:03:32 +0200

    rename tests

--- test/integration/statuspages.prod.spec.js
+++ test/integration/statuspages.prod.spec.js
@@ -3,49 +3,35 @@ const path = require('path');
 const request = require('supertest');
 const expect = require('expect');
 
-describe('Basics Dev', () => {
-  she('injects common.js into existing html', (done) => {
-    aden({ dev: true })
-      .init(path.resolve(__dirname, '../tmpdata/html'))
-      .then((an) => an.run('dev'))
-      .then((an) => {
-        request(an.app)
-          .get('/')
-          .end((err, res) => {
-            if (err) done(err);
-            expect(res.text).toMatch(/<script type="text\/javascript" src="\/commons\.js">/ig);
-            an.shutdown(done);
-          });
-      })
-      .catch(done);
-  });
-
-  she('routes custom status pages (404)', (done) => {
-    aden({ dev: true })
+describe('Statuspages Prod', () => {
+  she('does not route custom status pages (404)', (done) => {
+    aden()
       .init(path.resolve(__dirname, '../tmpdata/custom'))
-      .then((an) => an.run('dev'))
+      .then((an) => an.run('build'))
+      .then((an) => an.run('production'))
       .then((an) => {
         request(an.app)
           .get('/404')
           .end((err, res) => {
             if (err) done(err);
-            expect(res.text).toMatch(/Custom 404/ig);
+            expect(res.status).toMatch(404);
             an.shutdown(done);
           });
       })
       .catch(done);
   });
 
-  she('routes custom status pages (error)', (done) => {
-    aden({ dev: true })
+  she('does not route custom status pages (error)', (done) => {
+    aden()
       .init(path.resolve(__dirname, '../tmpdata/custom'))
-      .then((an) => an.run('dev'))
+      .then((an) => an.run('build'))
+      .then((an) => an.run('production'))
       .then((an) => {
         request(an.app)
           .get('/500')
           .end((err, res) => {
             if (err) done(err);
-            expect(res.text).toMatch(/Custom Error/ig);
+            expect(res.status).toMatch(404);
             an.shutdown(done);
           });
       })
@@ -53,9 +39,10 @@ describe('Basics Dev', () => {
   });
 
   she('uses custom status pages (404)', (done) => {
-    aden({ dev: true })
+    aden()
       .init(path.resolve(__dirname, '../tmpdata/custom'))
-      .then((an) => an.run('dev'))
+      .then((an) => an.run('build'))
+      .then((an) => an.run('production'))
       .then((an) => {
         request(an.app)
           .get('/not_a_page_in_path')
@@ -69,9 +56,10 @@ describe('Basics Dev', () => {
   });
 
   she('uses custom status pages (error)', (done) => {
-    aden({ dev: true })
+    aden()
       .init(path.resolve(__dirname, '../tmpdata/custom'))
-      .then((an) => an.run('dev'))
+      .then((an) => an.run('build'))
+      .then((an) => an.run('production'))
       .then((an) => {
         request(an.app)
           .get('/provoke')
@@ -83,8 +71,4 @@ describe('Basics Dev', () => {
       })
       .catch(done);
   });
-
-  she('// Things Aden already does but are untested...');
-  she('ignores dot files/folders for page parsing');
-  she('does not expose bare assets from page tree');
 });
