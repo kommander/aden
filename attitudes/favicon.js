106e96c15e74fd91059bf4b9d6d0161b7deb00a0
Sebastian Herrlinger <hasta84@gmail.com>
2017-05-06 18:53:19 +0200

    expose server in setup hook

--- attitudes/favicon.js
+++ attitudes/favicon.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 module.exports = (aden) => {
   aden.registerFile('favicon', /favicon\.ico$/);
 
-  aden.hook('setup', ({ rootPage, app }) => {
-    if (rootPage.key.favicon.resolved) {
-      const faviconRoute = `${rootPage.basePath}favicon.ico`;
-      const favicon = fs.readFileSync(rootPage.key.favicon.resolved);
+  aden.hook('setup', ({ app, pages }) => {
+    if (pages[0].key.favicon.resolved) {
+      const faviconRoute = `${pages[0].basePath}favicon.ico`;
+      const favicon = fs.readFileSync(pages[0].rootPage.key.favicon.resolved);
 
       app.use(faviconRoute, (req, res) => {
         res.setHeader('Content-Type', 'image/x-icon');
