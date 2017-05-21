96c6df9f6f3e2141674bec26f592fb6dd9d2e3a6
Sebastian Herrlinger <hasta84@gmail.com>
2017-05-21 03:52:41 +0200

    update CLI, fix false routes, rename config file

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 const createAden = require('./lib/aden');
-const express = require('express');
 const program = require('commander');
 const logger = require('./lib/aden.logger');
 const path = require('path');
@@ -15,15 +14,14 @@ const chalk = require('chalk');
 
 const collectAttitudes = (val, memo) => memo.concat(val);
 
+program._name = 'aden';
+
 program
-  .usage('[options] <rootpath ...>')
-  .option('-b, --build', 'Will only build out the app assets and exit (not start the server)')
-  .option('-d, --dev', 'Run in development mode (live reload)')
-  .option('-c, --clean', 'Remove all dist folders')
-  .option('-f, --focus [path]', 'Choose one route to focus on. Mount only that.')
+  .usage('<command> [rootPath] [options]')
+  .option('-f, --focus <path>', 'Choose one route to focus on. Mount only that.')
   .option('-w, --workers [num]', 'Start with given [num] of workers, or all CPUs.')
-  .option('-p, --port [port]', 'Override the port to mount the server on')
-  .option('-u, --use [attitude]', 'Specify an attitude to use (multi)', collectAttitudes, [])
+  .option('-p, --port <port>', 'Override the port to mount the server on')
+  .option('-u, --use <attitude>', 'Specify attitude(s) to use', collectAttitudes, [])
   .option('-s, --silent', 'Do not output anything on purpose')
   .option('-v, --verbose', 'Output a lot')
   .option('--debug', 'Debug output')
@@ -39,48 +37,21 @@ program
   .option('--log-no-date', 'Omit date from log output')
   .version(pckgJson.version);
 
-/* eslint-disable */
-program.on('--help', () => {
-  console.log('Issues and PRs welcome at https://github.com/kommander/aden');
-});
-/* eslint-enable */
+let log;
+let config;
 
-program.parse(process.argv);
+const resolveRootPath = (rootPath) => path.resolve('./', rootPath || '');
 
-const logOptions = {
-  silent: program.silent || false,
-  verbose: program.verbose || process.env.NODE_VERBOSE || false,
-  debug: program.debug || false,
-  noDate: !program.logDate || false,
+const numberOfWorkers = (workersById) => Object.keys(workersById)
+  .filter((key) => workersById.hasOwnProperty(key))
+  .length;
+
+const fatalErrorHandler = (err) => {
+  log.error('FATAL:', err, err._reason ? err._reason.stack : null);
+  if (process.env.NODE_ENV !== 'development' && !program.dev) {
+    process.exit(1);
+  }
 };
-
-const log = logger(logOptions).namespace('aden cli'); // eslint-disable-line
-
-if (program.dev) {
-  log.info(chalk.cyan('           _             '));
-  log.info(chalk.cyan('          | |            '));
-  log.info(chalk.cyan(' _____  __| |_____ ____  '));
-  log.info(chalk.cyan('(____ |/ _  | ___ |  _ \\ '));
-  log.info(chalk.cyan('/ ___ ( (_| | ____| | | |'));
-  log.info(chalk.cyan('\\_____|\\____|_____)_| |_|.zwerk.io'));
-  log.warn('Ahoy! Running in dev env.');
-} else {
-  log.info(`Running in ${process.env.NODE_ENV || 'production (by default)'} env.`);
-}
-
-const app = express();
-const config = {
-  logger: logOptions,
-  dev: program.dev || false,
-  attitudes: program.use,
-};
-
-const rootPath = path.resolve('./', program.args[0] || '');
-
-log.debug('cli config ', {
-  rootPath,
-  config,
-});
 
 const runServer = (aden, doOpen) => Promise.resolve().then(() => new Promise((resolve, reject) => {
   const splitPort = program.port
@@ -98,7 +69,7 @@ const runServer = (aden, doOpen) => Promise.resolve().then(() => new Promise((re
 
     const type = cluster.isMaster ? 'server' : 'worker';
 
-    aden.log.success(`Started ${type} at ${hostName || 'localhost'}:${port}`);
+    log.success(`Started ${type} at ${hostName || 'localhost'}:${port}`);
 
     if (doOpen) {
       open(`http://${hostName || 'localhost'}:${port}`);
@@ -108,115 +79,180 @@ const runServer = (aden, doOpen) => Promise.resolve().then(() => new Promise((re
   });
 }));
 
-const numberOfWorkers = (workersById) => Object.keys(workersById)
-  .filter((key) => workersById.hasOwnProperty(key))
-  .length;
-
-let run = null;
-
-if (program.build) {
-  run = createAden(app, config).init(rootPath, program.focus)
-    .then((aden) => aden.run('build'))
-    .then(() => {
-      log.success('Build only done. Exiting.');
-      process.exit(0);
-    });
-}
-
-if (!run && program.clean) {
-  run = createAden(app, config)
-    .init(rootPath, program.focus)
-    .then((aden) => aden.run('clean'))
-    .then(() => {
-      log.success('Clean up done. Exiting.');
-      process.exit(0);
-    });
-}
-
-if (!run && program.dev) {
-  process.traceDeprecation = true;
-  run = createAden(app, config)
-    .init(rootPath, program.focus)
-    .then((aden) => aden.run('dev'))
-    .then((aden) => runServer(aden));
-}
-
-if (!run) {
-  // Default clustering for production
-  if (program.workers && cluster.isMaster) {
-    const numCpus = require('os').cpus().length;
-    run = Promise.resolve().then(() => {
-      let max = parseInt(program.workers, 10) || numCpus;
-
-      if (max > numCpus) {
-        log.warn(`Starting more workers than CPUs available (${max}/${numCpus})`);
-        max = numCpus;
-      }
-
-      const workersById = {};
-      let numWorkersListening = 0;
-      let exitStatus = 0;
-
-      cluster.on('fork', (worker) => {
-        log.info(`Forked worker ${worker.id}`);
-        workersById[worker.id] = worker;
-
-        worker.on('error', (err) => {
-          log.error('Worker Error', err);
-        });
-
-        worker.on('exit', (code, signal) => {
-          delete workersById[worker.id];
-          numWorkersListening--;
-
-          if (code > 0) {
-            log.error('Worker Exit with Error', { code, signal });
-            exitStatus = 1;
-            // TODO: Determine if viable for restart
-          } else {
-            log.info('Worker Exit Normal', { code, signal });
-          }
-
-          const numWorkers = numberOfWorkers(workersById);
-          if (numWorkers === 0) {
-            log.info('No workers left, exiting');
-            process.exit(exitStatus);
-          }
-        });
-      });
-
-      cluster.on('listening', (worker, address) => {
-        log.success(`Worker ${worker.id} listening at ${address.address
-          || '127.0.0.1'}:${address.port}`);
-
-        numWorkersListening++;
-        if (numWorkersListening === max) {
-          log.success(`${numWorkersListening} workers listening at ${address.address
-            || '127.0.0.1'}:${address.port}`);
-        }
-      });
-
-      // Initial fork
-      for (let i = 0; i < max; i++) {
-        cluster.fork();
-      }
-    });
-  } else {
-    run = createAden(app, config)
-      .init(rootPath, program.focus)
-      .then((aden) => aden.run('production'))
-      .then((aden) => runServer(aden));
-  }
-}
-
-run.catch((err) => {
-  log.error('FATAL:', err, err._reason ? err._reason.stack : null);
-  if (process.env.NODE_ENV !== 'development' && !program.dev) {
-    process.exit(1);
-  }
+const deriveConfig = (prog, logOptions, dev) => ({
+  logger: logOptions,
+  attitudes: prog.use,
+  dev,
 });
 
+const getLogOptions = (prog) => ({
+  silent: prog.silent || false,
+  verbose: prog.verbose || process.env.NODE_VERBOSE || false,
+  debug: prog.debug || false,
+  noDate: !prog.logDate || false,
+});
+
+const initLogger = (dev, logOptions) => {
+  const log = logger(logOptions).namespace('aden cli'); // eslint-disable-line
+
+  if (cluster.isMaster) {
+    if (dev) {
+      log.info(chalk.cyan('           _             '));
+      log.info(chalk.cyan('          | |            '));
+      log.info(chalk.cyan(' _____  __| |_____ ____  '));
+      log.info(chalk.cyan('(____ |/ _  | ___ |  _ \\ '));
+      log.info(chalk.cyan('/ ___ ( (_| | ____| | | |'));
+      log.info(chalk.cyan('\\_____|\\____|_____)_| |_|.zwerk.io'));
+      log.warn('Ahoy! Running in dev env.');
+    } else {
+      log.info(`Running in ${process.env.NODE_ENV || 'production (by default)'} env.`);
+    }
+  }
+
+  return log;
+};
+
+const initLogAndConfig = (opts) => {
+  const logOptions = getLogOptions(program);
+  log = initLogger(opts.dev, logOptions);
+  config = deriveConfig(program, logOptions, opts.dev);
+};
+
+program
+  .command('start [rootPath]')
+  .alias('s')
+  .description('Run in production mode')
+  .action((rootPath) => {
+    initLogAndConfig({ dev: false });
+
+    // Default clustering for production
+    if (program.workers && cluster.isMaster) {
+      const numCpus = require('os').cpus().length;
+      Promise.resolve().then(() => {
+        let max = parseInt(program.workers, 10) || numCpus;
+
+        if (max > numCpus) {
+          log.warn(`Starting more workers than CPUs available (${max}/${numCpus})`);
+          max = numCpus;
+        }
+
+        const workersById = {};
+        let numWorkersListening = 0;
+        let exitStatus = 0;
+
+        cluster.on('fork', (worker) => {
+          log.info(`Forked worker ${worker.id}`);
+          workersById[worker.id] = worker;
+
+          worker.on('error', (err) => {
+            log.error('Worker Error', err);
+          });
+
+          worker.on('exit', (code, signal) => {
+            delete workersById[worker.id];
+            numWorkersListening--;
+
+            if (code > 0) {
+              log.error('Worker Exit with Error', { code, signal });
+              exitStatus = 1;
+              // TODO: Determine if viable for restart
+            } else {
+              log.info('Worker Exit Normal', { code, signal });
+            }
+
+            const numWorkers = numberOfWorkers(workersById);
+            if (numWorkers === 0) {
+              log.info('No workers left, exiting');
+              process.exit(exitStatus);
+            }
+          });
+        });
+
+        cluster.on('listening', (worker, address) => {
+          log.success(`Worker ${worker.id} listening at ${address.address
+            || '127.0.0.1'}:${address.port}`);
+
+          numWorkersListening++;
+          if (numWorkersListening === max) {
+            log.success(`${numWorkersListening} workers listening at ${address.address
+              || '127.0.0.1'}:${address.port}`);
+          }
+        });
+
+        // Initial fork
+        for (let i = 0; i < max; i++) {
+          cluster.fork();
+        }
+      });
+    } else {
+      createAden(Object.assign(config, { dev: false }))
+        .init(resolveRootPath(rootPath), program.focus)
+        .then((aden) => aden.run('production'))
+        .then((aden) => runServer(aden))
+        .catch(fatalErrorHandler);
+    }
+  });
+
+program
+  .command('dev [rootPath]')
+  .alias('d')
+  .description('Run in development mode (live reload)')
+  .action((rootPath) => {
+    initLogAndConfig({ dev: true });
+    createAden(config)
+      .init(resolveRootPath(rootPath), program.focus)
+      .then((aden) => aden.run('dev'))
+      .then((aden) => runServer(aden))
+      .catch(fatalErrorHandler);
+  });
+
+program
+  .command('build [rootPath]')
+  .alias('b')
+  .description('Will create a production build and exit')
+  .action((rootPath) => {
+    initLogAndConfig({ dev: false });
+    createAden(config)
+      .init(resolveRootPath(rootPath), program.focus)
+      .then((aden) => aden.run('build'))
+      .then(() => {
+        log.success('Build only done. Exiting.');
+        process.exit(0);
+      })
+      .catch(fatalErrorHandler);
+  });
+
+program
+  .command('clean [rootPath]')
+  .alias('c')
+  .description('Remove all dist folders')
+  .action((rootPath) => {
+    initLogAndConfig({ dev: false });
+    createAden(config)
+      .init(resolveRootPath(rootPath), program.focus)
+      .then((aden) => aden.run('clean'))
+      .then(() => {
+        log.success('Clean up done. Exiting.');
+        process.exit(0);
+      })
+      .catch(fatalErrorHandler);
+  });
+
+program
+  .command('deploy [rootPath] [target]')
+  .description('Run deploy task with default or given target(s)')
+  .action((rootPath, target, cmd) => {
+    log.info('deploy', rootPath, target, cmd);
+  });
+
+/* eslint-disable */
+program.on('--help', () => {
+  console.log('Issues and PRs welcome at https://github.com/kommander/aden');
+});
+/* eslint-enable */
+
 process.on('uncaughtException', (ex) => {
+  if (!log) initLogAndConfig({ dev: false });
   log.error('FATAL: Uncaught Exception', ex);
   if (process.env.NODE_ENV !== 'development' && !program.dev) {
     process.exit(1);
@@ -224,8 +260,11 @@ process.on('uncaughtException', (ex) => {
 });
 
 process.on('unhandledRejection', (reason) => {
+  if (!log) initLogAndConfig({ dev: false });
   log.error('FATAL: Unhandled Promise Rejection', reason);
   if (process.env.NODE_ENV !== 'development' && !program.dev) {
     process.exit(1);
   }
 });
+
+program.parse(process.argv);
