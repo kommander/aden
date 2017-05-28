7228665d5c467888fbbd2dd57e9c2a1cf57df898
Sebastian Herrlinger <hasta84@gmail.com>
2017-05-28 11:29:25 +0200

    unlink dir dev test, remove eventemitter from aden core

--- test/integration/dev.spec.js
+++ test/integration/dev.spec.js
@@ -3,10 +3,26 @@ const aden = require('../../lib/aden');
 const path = require('path');
 const request = require('supertest');
 const expect = require('expect');
+const rimraf = require('rimraf');
+const Logger = require('../../lib/aden.logger');
+const TestDuplex = require('../lib/test-duplex.js');
 
 describe('dev', () => {
   she('recognises new files and sets up the page', (done) => {
-    aden({ dev: true }).init(path.resolve(__dirname, '../tmpdata/dev'))
+    const stream = new TestDuplex();
+    const logParser = Logger.getLogParser();
+    logParser.attach(stream);
+
+    const adn = aden({
+      dev: true,
+      logger: {
+        silent: false,
+        stdStream: stream,
+        errStream: stream,
+      },
+    });
+
+    adn.init(path.resolve(__dirname, '../tmpdata/dev'))
       .then((an) => an.run('dev'))
       .then((an) => {
         request(an.app)
@@ -20,7 +36,7 @@ describe('dev', () => {
 
 
             // Mhhh... need to know when a build has finished
-            an.on('dev:reload:done', () => {
+            logParser.on('dev:reload:done', () => {
               request(an.app)
                 .get('/')
                 // fckn hell. todo: use promisified supertest
@@ -45,7 +61,20 @@ describe('dev', () => {
   });
 
   she('recognises new files in sub folders and sets up the page', (done) => {
-    aden({ dev: true }).init(path.resolve(__dirname, '../tmpdata/dev'))
+    const stream = new TestDuplex();
+    const logParser = Logger.getLogParser();
+    logParser.attach(stream);
+
+    const adn = aden({
+      dev: true,
+      logger: {
+        silent: false,
+        stdStream: stream,
+        errStream: stream,
+      },
+    });
+
+    adn.init(path.resolve(__dirname, '../tmpdata/dev'))
       .then((an) => an.run('dev'))
       .then((an) => {
         request(an.app)
@@ -58,7 +87,7 @@ describe('dev', () => {
             expect(res.status).toMatch(404);
 
             // Mhhh... need to know when a build has finished
-            an.on('dev:reload:done', () => {
+            logParser.on('dev:reload:done', () => {
               request(an.app)
                 .get('/sub/')
                 // fckn hell. todo: use promisified supertest
@@ -82,8 +111,71 @@ describe('dev', () => {
       });
   });
 
+  she('recognises deleted folders and removes the page', (done) => {
+    const stream = new TestDuplex();
+    const logParser = Logger.getLogParser();
+    logParser.attach(stream);
+
+    const adn = aden({
+      dev: true,
+      logger: {
+        silent: false,
+        stdStream: stream,
+        errStream: stream,
+      },
+    });
+
+    adn.init(path.resolve(__dirname, '../tmpdata/devunlink'))
+      .then((an) => an.run('dev'))
+      .then((an) => {
+        request(an.app)
+          .get('/sub/')
+          .end((err, res) => {
+            if (err) {
+              done(err);
+              return;
+            }
+            expect(res.status).toMatch(200);
+
+            // Mhhh... need to know when a build has finished
+            logParser.on('dev:reload:done', () => {
+              request(an.app)
+                .get('/sub/')
+                // fckn hell. todo: use promisified supertest
+                .end((err2, res2) => {
+                  if (err2) {
+                    done(err2);
+                    return;
+                  }
+
+                  expect(res2.status).toMatch(404);
+
+                  an.shutdown(done);
+                });
+            });
+
+            setTimeout(() => rimraf.sync(
+              path.resolve(__dirname, '../tmpdata/devunlink/sub')
+            ), 300);
+          });
+      });
+  });
+
   she('recognises changed watch keys', (done) => {
-    aden({ dev: true }).init(path.resolve(__dirname, '../tmpdata/dev2'))
+    const stream = new TestDuplex();
+    const logParser = Logger.getLogParser();
+    logParser.attach(stream);
+
+    const adn = aden({
+      dev: true,
+      logger: {
+        silent: false,
+        stdStream: stream,
+        errStream: stream,
+      },
+    });
+
+    adn.init(path.resolve(__dirname, '../tmpdata/dev2'))
       .then((an) => an.run('dev'))
       .then((an) => {
         request(an.app)
@@ -96,7 +188,7 @@ describe('dev', () => {
             expect(res.status).toMatch(404);
 
             // Mhhh... need to know when a build has finished
-            an.on('dev:reload:done', () => {
+            logParser.on('dev:reload:done', () => {
               request(an.app)
                 .get('/')
                 // fckn hell. todo: use promisified supertest
