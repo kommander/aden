eb7f34b163de7359ce174b5a40d7714f215d0c84
annoying bastard <annoying.bastard@gmx.de>
2017-07-24 11:53:56 +0200

    add google analytics

--- docs/index.js
+++ docs/index.js
@@ -3,6 +3,14 @@ const authorElement = document.querySelector('#author');
 const originElement = document.querySelector('#origin');
 const quoteButton = document.querySelector('#quoteButton');
 
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-103104194-1', 'auto');
+ga('send', 'pageview');
+
 const updateQuote = () => Promise.resolve()
   .then(() => quoteButton.removeEventListener('click', updateQuote))
   .then(() => fetch('/quotemachine/api/quote'))
